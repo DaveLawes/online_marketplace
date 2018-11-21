70a11f460c6ae70ef5e2d8cee1595677949df21a
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 11:07:17 +0000

    test: Item returns name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,10 +2,14 @@ require 'item'
 
 describe Item do
 
-  subject { described_class.new(1, 'chair', 9.25) }
+  subject { described_class.new(1, 'chair', 9.2) }
 
   it 'returns the product code' do
     expect(subject.product_code).to eq 1
+  end
+
+  it 'returns the name' do
+    expect(subject.name).to eq 'chair'
   end
 
 end
