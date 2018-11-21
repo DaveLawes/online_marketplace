711717b07d37271db16b8a09fe9a63028326970c
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 11:06:36 +0000

    feature: Item returns product code

---
+++ lib/Item.rb
@@ -0,0 +1,9 @@
+class Item
+
+  attr_reader :product_code
+
+  def initialize(product_code, name, price)
+    @product_code = product_code
+  end
+
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require 'item'
 
 describe Item do
 
-  subject { descrbed_class.new(1, 'chair', 9.25) }
+  subject { described_class.new(1, 'chair', 9.25) }
 
   it 'returns the product code' do
     expect(subject.product_code).to eq 1
