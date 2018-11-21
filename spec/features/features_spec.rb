369891ed7cc1b5614acad13ba4c6d84a2e7852a5
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 14:25:39 +0000

    test: added feature test for 1st user story

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,25 @@
+require 'checkout'
+
+describe 'Features' do
+
+  let(:co) { Checkout.new}
+  let(:item1) { Item.new(1, 'Very cheap chair', 9.25) }
+  let(:item2) { Item.new(2, 'Little table', 45) }
+  let(:item3) { Item.new(3, 'Funky light', 19.95) }
+
+  describe 'Checkout evaluates total price of basket' do
+
+    context 'given no promotional rules' do
+
+      it 'calculates the total price correctly' do
+        co.scan(item1)
+        co.scan(item2)
+        co.scan(item3)
+        expect(co.total).to eq '£74.20'
+      end
+
+    end
+
+  end
+
+end
