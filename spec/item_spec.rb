a9df5b7aa1303aa690accc97f95ab2ee26e6b49e
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 11:08:31 +0000

    test: Item returns price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require 'item'
 
 describe Item do
 
-  subject { described_class.new(1, 'chair', 9.2) }
+  subject { described_class.new(1, 'chair', 9.25) }
 
   it 'returns the product code' do
     expect(subject.product_code).to eq 1
@@ -10,6 +10,10 @@ describe Item do
 
   it 'returns the name' do
     expect(subject.name).to eq 'chair'
+  end
+
+  it 'returns the price' do
+    expect(subject.price).to eq 9.25
   end
 
 end
