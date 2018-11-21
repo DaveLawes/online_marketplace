b3ea27e2b9cf7a540a152799490e1c80d3c7a3d3
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 11:21:02 +0000

    test: #total returns total price

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -2,7 +2,7 @@ require 'checkout'
 
 describe Checkout do
 
-  let(:item) { double Item }
+  let(:item) { double Item, price: 9.25 }
 
   describe '#scan' do
 
@@ -10,6 +10,18 @@ describe Checkout do
       expect(subject.scan(item)[0]).to equal item
     end
 
+  end
+
+  describe '#total' do
+    context 'given no promotional_rules' do
+
+      it 'calculates the total price' do
+        subject.scan(item)
+        subject.scan(item)
+        expect(subject.total).to eq '£18.50'
+      end
+
+    end
   end
 
 end
