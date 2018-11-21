2603b2bbd2695b6752e0560f0d13221f79169bf8
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 14:28:21 +0000

    test: feature test for 2nd user story

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,20 +6,34 @@ describe 'Features' do
   let(:item1) { Item.new(1, 'Very cheap chair', 9.25) }
   let(:item2) { Item.new(2, 'Little table', 45) }
   let(:item3) { Item.new(3, 'Funky light', 19.95) }
+  let(:promotional_rules) { PromotionalRules.new }
 
   describe 'Checkout evaluates total price of basket' do
-
     context 'given no promotional rules' do
 
       it 'calculates the total price correctly' do
         co.scan(item1)
         co.scan(item2)
         co.scan(item3)
+
         expect(co.total).to eq '£74.20'
       end
 
     end
 
+    context 'given promotional rules' do
+
+      let(:co) { Checkout.new(promotional_rules) }
+
+      it 'calculates the correct total' do
+        co.scan(item1)
+        co.scan(item2)
+        co.scan(item3)
+
+        expect(co.total).to eq '£66.78'
+      end
+
+    end
   end
 
 end
