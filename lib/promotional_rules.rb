56191f2636a5f577f55a7afa5fc9faec66ce8a54
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 14:39:08 +0000

    refactor: promotional rules

--- lib/promotional_rules.rb
+++ lib/promotional_rules.rb
@@ -2,9 +2,7 @@ class PromotionalRules
 
   def change_unit_price(mapped_basket)
     mapped_basket.each do |item, quantity|
-      if quantity >=2
-        item.price = 8.5
-      end
+      item.price = 8.5 if quantity >=2
     end
     mapped_basket
   end
