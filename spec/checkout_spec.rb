c683ef30ad32e5220c03b23b8d486e0bde25318d
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 14:48:59 +0000

    test: checkout with promotional rules

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -22,6 +22,18 @@ describe Checkout do
       end
 
     end
+
+    context 'given promotional_rules' do
+      let(:promotional_rules) { double PromotionalRules }
+      subject { described_class.new(promotional_rules) }
+
+      it 'calculates the correct total price' do
+        subject.scan(item)
+        subject.scan(item)
+
+        expect(subject.total).to eq '£16.50'
+      end
+    end
   end
 
 end
