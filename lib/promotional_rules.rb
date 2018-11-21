41873ed94a4811bba91e8bc68d96a15d11382eb5
David Lawes <davidlawes@hotmail.co.uk>
2018-11-21 14:20:32 +0000

    feature: promotional rules changes unit price

--- lib/promotional_rules.rb
+++ lib/promotional_rules.rb
@@ -1,2 +1,12 @@
 class PromotionalRules
+
+  def change_unit_price(mapped_basket)
+    mapped_basket.each do |item, quantity|
+      if quantity >=2
+        item.price = 8.5
+      end
+    end
+    mapped_basket
+  end
+
 end
--- spec/promotional_rules_spec.rb
+++ spec/promotional_rules_spec.rb
@@ -2,15 +2,16 @@ require 'promotional_rules'
 
 describe PromotionalRules do
 
-  let(:item) { double Item, price: 9.25 }
-  let(:mapped_basket) { [ { item => 2 } ] }
+  let(:item) { Item.new(1, 'chair', 9.25) }
+  # let(:item) { double Item, price: 9.25 }
+  let(:mapped_basket) { { item => 2 } }
 
   describe '#change_unit_price' do
     context 'given the quantity exceeds the specified amount' do
 
       it 'updates the unit price' do
-        result = subject.change_unit_price(mapped_basket)
-        expect(result[0].price).to eq 8.5
+        subject.change_unit_price(mapped_basket)
+        expect(item.price).to eq 8.5
       end
 
     end
